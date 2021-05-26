f818b193e635e9fee3c52166420bc4c70b73e68a
Kerem <63135398+Kerem-git@users.noreply.github.com>
2021-05-27 02:31:54 +0300

    Update insta_asistant_.py

--- insta_asistant_.py
+++ insta_asistant_.py
@@ -7,7 +7,6 @@ import random
 
 
 a = 5 
-secret_key = input("google_auth_key: ")
 opts = Options()
 opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0")
 driver: WebDriver = webdriver.Chrome(ChromeDriverManager().install(),options=opts)
